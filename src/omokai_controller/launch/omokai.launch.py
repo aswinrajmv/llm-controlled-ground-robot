1ce28b02e226aedc54fa7fe31d33e6374a8515cb
Aswin <aswinrajmv777@gmail.com>
2026-08-18 19:02:55 +0530

    Make Gazebo simulation paths portable

--- src/omokai_controller/launch/omokai.launch.py
+++ src/omokai_controller/launch/omokai.launch.py
@@ -1,33 +1,55 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    package_share = get_package_share_directory(
+        'omokai_controller'
+    )
+
+    models_dir = os.path.join(
+        package_share,
+        'simulation',
+        'models'
+    )
+
+    world_file = os.path.join(
+        package_share,
+        'simulation',
+        'worlds',
+        'ground_robot_world.sdf'
+    )
+
+    resource_path = os.environ.get('GZ_SIM_RESOURCE_PATH', '')
+
+    if resource_path:
+        resource_path = f'{models_dir}:{resource_path}'
+    else:
+        resource_path = models_dir
+
+    gazebo_env = os.environ.copy()
+    gazebo_env['GZ_SIM_RESOURCE_PATH'] = resource_path
+    gazebo_env['LIBGL_ALWAYS_SOFTWARE'] = '1'
+
     return LaunchDescription([
 
-        # Start Gazebo simulation
+        # Start Gazebo simulation.
         ExecuteProcess(
             cmd=[
-                'bash',
-                '-c',
-                (
-                    'cd ~/omokai_ws && '
-                    'source /opt/ros/lyrical/setup.bash && '
-                    'source ~/omokai_ws/install/setup.bash && '
-                    'export GZ_SIM_RESOURCE_PATH='
-                    '$GZ_SIM_RESOURCE_PATH:'
-                    '$HOME/omokai_ws/src/omokai_controller/simulation/models && '
-                    'LIBGL_ALWAYS_SOFTWARE=1 '
-                    'gz sim -r '
-                    '~/omokai_ws/src/omokai_controller/'
-                    'simulation/worlds/omokai_world.sdf'
-                )
+                'gz',
+                'sim',
+                '-r',
+                world_file,
             ],
+            additional_env=gazebo_env,
             output='screen'
         ),
 
-        # Gazebo <-> ROS bridges
+        # Gazebo <-> ROS bridges.
         Node(
             package='ros_gz_bridge',
             executable='parameter_bridge',
